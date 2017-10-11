2c26bb4323f3e827694e3fb076b6accb31bd8bb7
Alan Falcon and Kimberly Bordon <alan+kimberly@>
2017-10-11 09:29:24 -0700

    New Todo via MVC and restful structure

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Todo.Models;
+using System.Collections.Generic;
+
+namespace Todo.Controllers
+{
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+    [HttpGet("/tasks")]
+    public ActionResult Tasks()
+    {
+      List<Task> allTasks = Task.GetAll();
+      return View(allTasks);
+    }
+    [HttpGet ("/tasks/new")]
+    public ActionResult TaskForm()
+    {
+      return View();
+    }
+    [HttpPost("/tasks")]
+    public ActionResult AddTask()
+    {
+      Task newTask = new Task(Request.Form["new-task"]);
+      List<Task> allTasks = Task.GetAll();
+      return View("Tasks", allTasks);
+    }
+    [HttpGet("/tasks/{id}")]
+    public ActionResult TaskDetail(int id)
+    {
+      Task task = Task.Find(id);
+      return View(task);
+    }
+  }
+}
